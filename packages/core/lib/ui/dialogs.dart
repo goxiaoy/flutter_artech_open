b9a613675830f415c219e9edc4091b77c1049b4e
Charles <charles@cognitivateleap.com>
2021-03-03 13:31:27 -0800

    implemented context show toast

--- packages/core/lib/ui/dialogs.dart
+++ packages/core/lib/ui/dialogs.dart
@@ -14,21 +14,65 @@ void showMessageSnackBar(BuildContext context,
   ));
 }
 
+
 Future<bool> showToast(String message,
-    {MessageType type = MessageType.message}) async {
-  return await Fluttertoast.showToast(
-      msg: message,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: type.toColor(),
-      textColor: Colors.white,
-      fontSize: 16.0);
+    {MessageType type = MessageType.success,
+      double iconsSize = 100.0,
+      double fontSize = 16.0,
+      bool shortMessage = true,
+      Color textColor = Colors.white}) async {
+  ArgumentError.checkNotNull(message);
+
+  if (FToast().context == null) {
+    return await Fluttertoast.showToast(
+        msg: message,
+        toastLength: shortMessage ? Toast.LENGTH_SHORT : Toast.LENGTH_LONG,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: type.toColor(),
+        textColor: textColor,
+        fontSize: fontSize);
+  } else {
+    FToast().showToast(
+        toastDuration: shortMessage
+            ? const Duration(seconds: 2)
+            : const Duration(seconds: 5),
+        gravity: ToastGravity.CENTER,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            color: type.toColor(),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if(type == MessageType.success)
+                Icon(Icons.check_circle_outlined, color: textColor,
+                  size: iconsSize,),
+              if(type == MessageType.error)
+                Icon(Icons.error_outline_rounded, color: textColor,
+                    size: iconsSize),
+              if(type == MessageType.warn)
+                Icon(Icons.warning_amber_outlined, color: textColor,
+                    size: iconsSize),
+              if(type != MessageType.message)
+                const SizedBox(
+                  height: 12.0,
+                ),
+              Text(message,
+                style: TextStyle(color: textColor, fontSize: fontSize),),
+            ],
+          ),
+        ));
+    return Future.value(true);
+  }
 }
 
 enum MessageType {
   message,
   warn,
+  success,
   error,
 }
 
@@ -38,6 +82,7 @@ extension MessageTypeExtension on MessageType {
   Color toColor() {
     switch (this) {
       case MessageType.message:
+      case MessageType.success:
         return Colors.black;
       case MessageType.warn:
         return Colors.orange[400]!;
@@ -49,3 +94,4 @@ extension MessageTypeExtension on MessageType {
     }
   }
 }
+
