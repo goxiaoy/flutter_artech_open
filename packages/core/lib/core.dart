024f80a4b62de338b1439bf25d77988001c41258
Charles <charles@cognitivateleap.com>
2021-03-27 16:55:39 -0700

    gender is basic data

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -21,6 +21,7 @@ export 'package:artech_core/time/date_time_extension.dart';
 export 'package:artech_core/ui/ui.dart';
 export 'package:artech_core/utils/utils.dart';
 export 'package:artech_core/ui/navigation_service.dart';
+export 'package:artech_core/data/gender.dart';
 
 export 'services_extension.dart';
 
