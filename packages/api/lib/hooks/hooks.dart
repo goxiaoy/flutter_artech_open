2b1dbfa86999ff195507cff4a62e55107d74bad9
Goxiaoy <xy1996mail@gmail.com>
2021-04-14 23:42:51 +0800

    remove params

--- packages/api/lib/hooks/hooks.dart
+++ packages/api/lib/hooks/hooks.dart
@@ -1,13 +1,11 @@
 import 'package:artech_core/core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 
 import '../api.dart';
 
 AsyncSnapshot<T> useMemoizedStreamProvider<T>(
     StreamProvider<T> Function() streamProviderBuilder,
-    T Function(QueryResult) deserializeFunc,
     [List<Object> keys = const <Object>[]]) {
   final streamProvider = useMemoized(streamProviderBuilder, keys);
   useEffect(
