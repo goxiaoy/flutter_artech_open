85e185ecdc8d684e5426a5d2399753d1b52530fe
Goxiaoy <xy1996mail@gmail.com>
2021-04-14 17:41:25 +0800

    fix web timezone

--- packages/core/lib/time/time_module.dart
+++ packages/core/lib/time/time_module.dart
@@ -1,8 +1,10 @@
 import 'package:artech_core/app_module_base.dart';
 import 'package:artech_core/core.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:logging/logging.dart';
+import 'package:time_machine/time_machine.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -27,7 +29,8 @@ class TimeModule extends AppSubModuleBase {
 
   Future<void> setCurrentZoneTime() async {
     try {
-      tz.initializeTimeZones(); // There is no checking, call this anyway
+      tz.initializeTimeZones(); //
+
       if (!kIsWeb) {
         //TODO GMT can not be used https://github.com/pinkfish/flutter_native_timezone/issues/15
         final String currentTimeZone = await executeWithStopwatch(
@@ -35,9 +38,25 @@ class TimeModule extends AppSubModuleBase {
             name: 'FlutterNativeTimezone.getLocalTimezone');
         _log.info('Time zone: $currentTimeZone');
         tz.setLocalLocation(tz.getLocation(currentTimeZone));
+      } else {
+        await _loadFromTimeMachine();
       }
     } catch (error) {
       _log.severe('configureLocalTimeZone error: $error');
+      await _loadFromTimeMachine();
+    }
+  }
+
+  Future<void> _loadFromTimeMachine() async {
+    try {
+      await executeWithStopwatch(() async {
+        await TimeMachine.initialize(
+            <String, dynamic>{'rootBundle': rootBundle});
+      }, name: 'Dart Time Machine Init');
+      _log.info('Hello, ${DateTimeZone.local} from the Dart Time Machine!\n');
+      tz.setLocalLocation(tz.getLocation(DateTimeZone.local.toString()));
+    } catch (error) {
+      _log.severe('loadFromTimeMachine error: $error');
     }
   }
 }
