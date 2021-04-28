23ea6e688863f0eae04d216dcfd80752bc4e7cae
Goxiaoy <xy1996mail@gmail.com>
2021-04-28 14:42:54 +0800

    add HasNamedLogger

--- packages/core/lib/logging/logger_mixin.dart
+++ packages/core/lib/logging/logger_mixin.dart
@@ -1,5 +1,17 @@
+import 'package:artech_core/core.dart';
 import 'package:logging/logging.dart';
 
+@Deprecated('Use HasNamedLogger instead')
 mixin HasSelfLoggerTyped<T> {
   final Logger logger = Logger(T.toString());
+}
+
+mixin HasNamedLogger {
+  late final String loggerName;
+
+  Logger? _logger;
+  Logger get logger {
+    return getOr<Logger?>(() => _logger, () => Logger(loggerName),
+        setter: (Logger? l) => _logger = l)!;
+  }
 }
