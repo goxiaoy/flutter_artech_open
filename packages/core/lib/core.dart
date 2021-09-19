69edf65339476101ed3dd7b975e8b7fca8c353ca
Goxiaoy <xy1996mail@gmail.com>
2021-09-19 12:37:23 +0800

    Revert "update version package"
    This reverts commit 5197c1eb3e7f46eb411489a301b189a2016c09bd.

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -29,7 +29,6 @@ export 'package:multiple_localization/multiple_localization.dart';
 export 'package:timezone/timezone.dart';
 export 'package:flutter_native_timezone/flutter_native_timezone.dart';
 export 'package:universal_platform/universal_platform.dart';
-export 'package:version/version.dart';
 
 export 'event/event_emitter.dart';
 export 'services_extension.dart';
