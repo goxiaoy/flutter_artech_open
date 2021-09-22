750e1e05452a2809ba7c8a48277ce79d8e3798e4
Goxiaoy <xy1996mail@gmail.com>
2021-09-22 12:07:33 +0800

    remove emitter

--- packages/api/lib/emitter/client_stub.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mqtt_client/mqtt_client.dart';
-
-MqttClient getClient(
-  String server,
-  String clientIdentifier, {
-  int maxConnectionAttempts = 3,
-}) =>
-    throw UnsupportedError('Cannot create client');
