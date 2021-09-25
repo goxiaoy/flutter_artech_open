4debfcefba2992755af7c3512a65e0e81502e944
Goxiaoy <xy1996mail@gmail.com>
2021-09-26 00:19:33 +0800

    add IgnoreCertHttpOverrides

--- packages/api/lib/api.dart
+++ packages/api/lib/api.dart
@@ -8,6 +8,7 @@ export 'hooks/hooks.dart';
 export 'http/auth_interceptor.dart';
 export 'http/dio_options.dart';
 export 'http/http_repo.dart';
+export 'http/ignore_cert_http_override.dart';
 export 'model.dart';
 export 'stream_provider.dart';
 export 'ui/health_provider.dart';
