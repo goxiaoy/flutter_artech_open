135d2f9ebe48ce7560016391a31a3e8b7826b433
Charles <shao8843@yahoo.com>
2022-01-27 17:45:21 -0800

    Use scaffoldBuilder

--- packages/core/lib/ui/dev/module_visualization.dart
+++ packages/core/lib/ui/dev/module_visualization.dart
@@ -1,6 +1,5 @@
-import 'package:artech_core/app_module_base.dart';
 import 'package:artech_core/core.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:artech_ui_kit/ui_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:graphview/GraphView.dart';
 
@@ -26,7 +25,7 @@ class _ModuleVisualizationState extends State<ModuleVisualization> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
+        child: scaffoldBuilder(context,
             body: Column(
       mainAxisSize: MainAxisSize.max,
       children: [
